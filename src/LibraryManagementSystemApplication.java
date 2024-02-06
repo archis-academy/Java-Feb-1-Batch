568ef212a193c2a11fa718bae921f61be5b6bef9
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-02-06 19:45:59 +0300

    [JA-09] - add arrays

--- src/LibraryManagementSystemApplication.java
+++ src/LibraryManagementSystemApplication.java
@@ -1,4 +1,9 @@
 public class LibraryManagementSystemApplication {
+    static int INDEX = 100;
+    static String[][] books = new String[INDEX][4];
+    static String[][] patrons = new String[INDEX][4];
+    static String[][] transactions = new String[INDEX][3];
+
     public static void main(String[] args) {
     }
 }
